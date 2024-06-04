f920c7a7fffe38309d16f6fe7a6327c6f35d43ba
Herbie216 <150303988+Herbie216@users.noreply.github.com>
2024-06-04 12:29:55 -0400

    All Test Passing

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,73 @@
-// DO YOUR MAGIC
+const express = require('express')
+const router = express.Router()
+const Car = require('./cars-model')
+const {
+    checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique
+} = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => { // eslint-disable-line
+    res.json(req.car)
+})
+
+router.post(
+    '/',
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+    async (req, res, next) => {
+        try {
+            const car = await Car.create(req.body)
+            res.json(car)
+        } catch (err) {
+            next(err)
+        }
+    })
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = router
+
+// Task 3: Stretch Problems
+// Add seed data to the database using knex seeds
+// Add [PUT] and [DELETE] operations to your API.
+// Write a schema file for a sales table. 
+// This table should track information on the sale of each car. 
+// You may wish to research foreign keys in order 
+// to link each sale to the entry in cars which sold.
+
+//                                  add optionally
+
+// router.put('/:id',
+//     md.checkAccountId,
+//     md.checkAccountPayload,
+//     async (req, res, next) => {
+//         try {
+//             const updated = await Account.updateById(req.params.id, req.body)
+//             res.json(updated)
+//         } catch (err) {
+//             next(err)
+//         }
+//     });
+
+// router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+//     try {
+//         await Account.deleteById(req.params.id)
+//         res.json(req.account)
+//     } catch (err) {
+//         next(err)
+//     }
+// })
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
-// STRETCH
+const cars = [
+    {
+        vin: '11111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 210000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '11111111111112',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 100000,
+        title: 'salvage',
+    },
+    {
+        vin: '11111111111113',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    },
+
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
